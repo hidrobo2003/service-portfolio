ecc87ffd09e3515410ffbaf7f04d5111c0f63cb5
Andrés Hidrobo <andresmhidroboe@gmail.com>
2026-06-02 17:33:09 -0500

    Add CORS policy to allow GitHub Pages

--- Program.cs
+++ Program.cs
@@ -8,7 +8,15 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 
 // ─── Services ───────────────────────────────────────────────────────────────
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowGitHubPages", policy =>
+    {
+        policy.WithOrigins("https://hidrobo2003.github.io")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
         options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
@@ -30,7 +38,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
-
+app.UseCors("AllowGitHubPages");
 //app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
