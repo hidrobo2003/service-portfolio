26582217819b790f02d40abe1162666e39c10f8e
Andrés Hidrobo <andresmhidroboe@gmail.com>
2026-06-02 19:17:52 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,11 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowGitHubPages", policy =>
     {
-        policy.WithOrigins("https://hidrobo2003.github.io","http://localhost:5500")
+        policy.WithOrigins("https://hidrobo2003.github.io",  // producción (sin cambios)
+    "http://localhost:5500",           // Live Server
+    "http://127.0.0.1:5500",          // Live Server (alternativo)
+    "http://localhost:3000",           // por si usas otro puerto
+    "http://127.0.0.1:3000")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
